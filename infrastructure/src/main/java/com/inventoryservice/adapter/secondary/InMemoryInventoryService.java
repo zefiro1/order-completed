6cdab2c62e84b2d4c64b9043e27cbd7632c390f9
Jose Antonio Fernandez Gonzalez <jose.fg@globant.com>
2024-12-04 15:25:37 +0100

    📝 (infrastructure/src/main/java/com/inventoryservice/adapter/secondary): Implement InMemoryInventoryService and InMemoryProductRepository for secondary port adapters
    This commit introduces two new classes, `InMemoryInventoryService` and `InMemoryProductRepository`, which implement the secondary ports of the inventory service. These classes are used to interact with the application's domain layer using an in-memory data structure instead of a real database. This change allows for easier testing and development without the need for a separate database setup.

---
+++ infrastructure/src/main/java/com/inventoryservice/adapter/secondary/InMemoryInventoryService.java
@@ -0,0 +1,32 @@
+package com.inventoryservice.adapter.secondary;
+
+
+import com.inventoryservice.domain.service.ProductDomainService;
+import com.inventoryservice.ports.secondary.InventoryService;
+import com.inventoryservice.ports.secondary.ProductRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryInventoryService implements InventoryService {
+  private final Map<String, Integer> inventory = new HashMap<>();
+  private final ProductRepository productRepository;
+  private final ProductDomainService productDomainService;
+
+  public InMemoryInventoryService(ProductRepository productRepository, ProductDomainService productDomainService) {
+    this.productRepository = productRepository;
+    this.productDomainService = productDomainService;
+    reloadInventory();
+  }
+
+  @Override
+  public void reduceStock(String productId, int quantity) {
+    productDomainService.reduceStock(productId, quantity);
+  }
+
+  @Override
+  public void reloadInventory() {
+    inventory.clear();
+    productRepository.findAll().forEach(product -> inventory.put(product.getId(), product.getStock()));
+  }
+}
