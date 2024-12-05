c81c5daad067875a15d54d39c2d4968b33451039
Jose Antonio Fernandez Gonzalez <jose.fg@globant.com>
2024-12-05 10:21:25 +0100

    🔧 Update CommandQueryBus to use type inference for generic types

--- application/src/main/java/com/inventoryservice/dispatcher/CommandQueryBus.java
+++ application/src/main/java/com/inventoryservice/dispatcher/CommandQueryBus.java
@@ -5,11 +5,11 @@ import com.inventoryservice.handlers.command.CommandHandler;
 import com.inventoryservice.handlers.query.QueryHandler;
 
 public class CommandQueryBus {
-  public <T> void dispatchCommand(T command, CommandHandler<T> handler) {
-    handler.handle(command);
-  }
+    public <T> void dispatchCommand(T command, CommandHandler<T> handler) {
+        handler.handle(command);
+    }
 
-  public <Q, R> R dispatchQuery(Q query, QueryHandler<Q, R> handler) {
-    return handler.handle(query);
-  }
+    public <Q, R> R dispatchQuery(Q query, QueryHandler<Q, R> handler) {
+        return handler.handle(query);
+    }
 }
