940569446989e3f1d56cfee571f4da702ef0a0d9
Jose Antonio Fernandez Gonzalez <jose.fg@globant.com>
2024-12-05 10:32:21 +0100

    🔧: Add Spring Boot Application and ServletInitializer for Inventory Service App

---
+++ infrastructure/src/main/java/com/inventoryservice/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.inventoryservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(InventoryServiceApp.class);
+    }
+}
