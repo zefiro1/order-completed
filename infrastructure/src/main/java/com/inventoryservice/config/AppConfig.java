4409b66f69fb05f67906febedd357377f39a0d22
Jose Antonio Fernandez Gonzalez <jose.fg@globant.com>
2024-12-16 12:04:29 +0100

    🐛 Implement RabbitMQ for communication between services (#123)
    * Added necessary imports and configurations to enable RabbitMQ in the application.
    * Created a queue, binding, exchange, and message converter beans.
    * Configured RabbitTemplate with the message converter.
    
    Reason: To improve the communication efficiency between services by using RabbitMQ instead of direct service calls. This will help to decouple the services and make the system more scalable and resilient.

--- infrastructure/src/main/java/com/inventoryservice/config/AppConfig.java
+++ infrastructure/src/main/java/com/inventoryservice/config/AppConfig.java
@@ -8,13 +8,52 @@ import com.inventoryservice.ports.secondary.InventoryService;
 import com.inventoryservice.ports.secondary.NotificationService;
 import com.inventoryservice.ports.secondary.ProductRepository;
 import com.inventoryservice.services.ManageInventoryService;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@EnableRabbit
 public class AppConfig {
+
+    static final String EXCHANGE = "inventory.exchange";
+    public static final String PRODUCT_REQUEST_QUEUE = "product.request.queue";
+    @Bean
+    public Queue queue() {
+        return new Queue(PRODUCT_REQUEST_QUEUE, false);
+    }
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("product.routing.key");
+    }
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        final var template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
 
     @Bean
     public ProductRepository productRepository() {
