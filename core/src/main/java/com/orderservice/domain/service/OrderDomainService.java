47dbc8cba78d795f59a2a0a70efc8e5f73e20ed3
Jose Antonio Fernandez Gonzalez <jose.fg@globant.com>
2024-12-16 12:28:51 +0100

    🐛 Fix a bug in OrderDomainService: calculateTotalAmount method now correctly calculates the total amount of an order by requesting product prices from the inventory service using RabbitMQ and considering the quantity of each item in the order. ✨ Introduce new feature in NotificationService: ability to send notifications about order status changes. ♻️ Refactor code in OrderDomainService: added dependencies for AmqpTemplate, ProductRequestMessage, and ProductResponseMessage classes, and updated calculateTotalAmount method to use RabbitMQ for product price requests. 🔧 Add configuration files: application.properties file with RabbitMQ connection details.

--- core/src/main/java/com/orderservice/domain/service/OrderDomainService.java
+++ core/src/main/java/com/orderservice/domain/service/OrderDomainService.java
@@ -1,9 +1,34 @@
 package com.orderservice.domain.service;
 
+import com.orderservice.domain.event.ProductRequestMessage;
+import com.orderservice.domain.event.ProductResponseMessage;
+import com.orderservice.domain.model.Order;
 import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
+@Service
 public class OrderDomainService {
+     private final AmqpTemplate amqpTemplate;
 
+    public double calculateTotalAmount(Order order) {
+        List<ProductResponseMessage> productResponses = order.getItems().keySet().stream()
+                .map(orderItem -> {
+                    ProductRequestMessage request = new ProductRequestMessage(orderItem);
+                    return (ProductResponseMessage) amqpTemplate.convertSendAndReceive("inventory.exchange", "product.routing.key", request);
+                })
+                .toList();
+
+        double totalAmount = 0;
+        for (ProductResponseMessage response : productResponses) {
+            double productPrice = response.getPrice();
+            int quantity = order.getItems().get(response.getProductId()).getQuantity();
+            totalAmount += productPrice * quantity;
+        }
+        return totalAmount;
+    }
 
 }
