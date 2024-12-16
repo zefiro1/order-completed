bb1b9bcf87df175230f24620f2b2660c2c372078
Jose Antonio Fernandez Gonzalez <jose.fg@globant.com>
2024-12-16 12:04:07 +0100

    📝 (infrastructure/src/main/java/com/inventoryservice/adapter/secondary): Refactor OrderEventConsumer to handle ProductPriceEvent and use Spring AMQP annotations for RabbitMQ consumption.
    The OrderEventConsumer class was refactored to process the ProductPriceEvent instead of OrderCompletedEvent, and it now uses Spring AMQP annotations for RabbitMQ consumption. This change allows the Inventory Service to handle product price requests from other microservices more efficiently.

--- infrastructure/src/main/java/com/inventoryservice/adapter/secondary/OrderEventConsumer.java
+++ infrastructure/src/main/java/com/inventoryservice/adapter/secondary/OrderEventConsumer.java
@@ -1,59 +1,22 @@
-package com.ordercompleted.adapter.secondary;
+package com.inventoryservice.adapter.secondary;
 
-import com.ordercompleted.domain.event.OrderCompletedEvent;
-import com.ordercompleted.ports.primary.OrderServiceUseCase;
-import com.ordercompleted.ports.secondary.OrderCompletedEventProcessor;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+
+import com.inventoryservice.domain.event.ProductRequestMessage;
+import com.inventoryservice.domain.event.ProductResponseMessage;
+import com.inventoryservice.domain.model.Product;
+import com.inventoryservice.ports.secondary.ProductPriceEventProcessor;
+import com.inventoryservice.ports.secondary.ProductRepository;
 import lombok.AllArgsConstructor;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeoutException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
-public class OrderEventConsumer implements OrderCompletedEventProcessor {
-  private static final String QUEUE_NAME = "order_completed_queue";
-  private static final String RABBITMQ_HOST = "localhost";
-  private static final String USERNAME = "user";
-  private static final String PASSWORD = "password";
-  private static final String VIRTUAL_HOST = "/";
-  private final OrderServiceUseCase orderServiceUseCase;
-  @Override
-  public void processOrderCompletedEvent(OrderCompletedEvent event) {
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost(RABBITMQ_HOST);
-    factory.setUsername(USERNAME);
-    factory.setPassword(PASSWORD);
-    factory.setVirtualHost(VIRTUAL_HOST);
-
-    try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-
-      channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-      System.out.println(" [*] Esperando mensajes en la cola '" + QUEUE_NAME + "'...");
-
-      DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-        String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-        System.out.println(" [x] Recibido: '" + message + "'");
-
-        processOrderCompletedEvent(message);
-      };
-
-      channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
-      });
-
-    } catch (IOException | TimeoutException e) {
-      e.printStackTrace();
+@Service
+public class OrderEventConsumer implements ProductPriceEventProcessor {
+    private final ProductRepository productRepository;
+    @RabbitListener(queues = "product.request.queue")
+    public ProductResponseMessage getProductPrice(ProductRequestMessage request) {
+        Product product = productRepository.findById(request.getProductId());
+        return new ProductResponseMessage(product.getId(), product.getPrice());
     }
-  }
-  private void processOrderCompletedEvent(String message) {
-    String orderId = extractOrderIdFromMessage(message);
-    orderServiceUseCase.markOrderAsCompleted(orderId);
-  }
-  private String extractOrderIdFromMessage(String message) {
-    return message.split(":")[1].trim();
-  }
-
 }
